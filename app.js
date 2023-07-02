a3e7eaf4aa578ed8ba5269af211dc2721a70baa4
SerraJunior123 <98670408+SerraJunior123@users.noreply.github.com>
2023-07-02 13:33:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+const abrir=()=>{
+
+
+ const open=document.querySelector('.CONTAINER')
+const contbt=document.querySelectorAll('.btn')
+   
+
+
+open.style.width='250px';
+contbt.style.display='none'
+
+
+
+   
+  
+  
+
+
+  
+
+}
